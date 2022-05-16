1831a51d35b820d04f923eb002b672bcedb73af9
nxuanthanh <n.xuanthanhh@gmail.com>
2022-05-16 11:50:31 +0700

    update:  - make experience  - make services  - responsive

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,10 +1,107 @@
 import React from 'react';
+import { BsPatchCheckFill } from 'react-icons/bs';
 import './experience.scss';
 function Experience(props) {
   return (
     <section id="experience">
       <h5>What skill I have</h5>
       <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Developer</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>TypeScript</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>ReactJs</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Redux</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Material-UI</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Firebase</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Figma</h4>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="backend__frontend">
+          <h3>Backend Developer</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NodeJS</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>SQL Server</h4>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Java</h4>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,7 +1,65 @@
 import React from 'react';
-
+import { BiCheck } from 'react-icons/bi';
+import './services.scss';
 function Services(props) {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__header">
+            <h3>Front-end Web Developer</h3>
+          </div>
+
+          <ul className="service__list">
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">Working environment is professional, sociable.</p>
+            </li>
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">
+                The basic regimes such as insurance, salary and bonus are clear.
+              </p>
+            </li>
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">
+                Official salary (after internship) from 8 million VND.
+              </p>
+            </li>
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">Job advancement route clear and easonable.</p>
+            </li>
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">Guidance and help for development.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__header">
+            <h3>Fullstack Web Developer (in the future)</h3>
+          </div>
+
+          <ul className="service__list">
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">Same with Front-end role.</p>
+            </li>
+            <li className="service__item">
+              <BiCheck className="service__icon" />
+              <p className="service__text">Better salary.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 Services.propTypes = {};
